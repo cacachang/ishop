9044d9bb77955669903e72d90cbe1d1ec73d3233
Ning <a24701770@gmail.com>
2023-08-27 02:19:14 +0800

    add: confirmation mail to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def confirmation_instructions(record, token, opts={})
+    @user = record
+    @url  = 'http://localhost:3000'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
