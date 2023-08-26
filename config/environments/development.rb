4604f0c58fcaa64fc25293fb94701d8f904b297a
Ning <a24701770@gmail.com>
2023-08-27 02:19:14 +0800

    add: confirmation mail to user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'ishop',
+    user_name:            ENV['MAILGUN_USERNAME'],
+    password:             ENV['MAILGUN_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
